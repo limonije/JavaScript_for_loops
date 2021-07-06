377767573a2eed1e199f96a986c670fc86add63d
limonije <mroozen@live.nl>
2021-07-06 18:20:19 +0200

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,16 @@
+for (let i = 0; i <= 20; i++) {
+  console.log(`Is ${i} even or odd?`);
+  if (i % 2 == 0) {
+    console.log(`${i} is even`);
+  } else {
+    console.log(`${i} is oneven`);
+  }  
+}
+
+// Expected output:
+// 1 is oneven 
+// 2 is even 
+// 3 is oneven 
+// 4 is even 
+// ...
+
